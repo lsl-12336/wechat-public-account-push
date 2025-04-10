d8e5ddda3e6b9ef928ca79339d456e42b0d7423c
lsl-12336 <58540626+lsl-12336@users.noreply.github.com>
2025-04-10 11:33:05 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -6,34 +6,49 @@ const TEMPLATE_CONFIG = [
     desc: `
       **{{date.DATA}}**
       
-      下个休息日：{{holidaytts.DATA}}
+    //   下个休息日：{{holidaytts.DATA}}
       
-      ---
+    //   ---
       
-      城市：{{city.DATA}}
+    //   城市：{{city.DATA}}
       
-      天气：{{weather.DATA}}
+    //   天气：{{weather.DATA}}
       
-      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+    //   气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
       
-      风向: {{wind_direction.DATA}}
+    //   风向: {{wind_direction.DATA}}
       
-      风级: {{wind_scale.DATA}}
+    //   风级: {{wind_scale.DATA}}
       
-      {{comprehensive_horoscope.DATA}}
+    //   {{comprehensive_horoscope.DATA}}
       
-      ---
+    //   ---
       
-      今天是我们相识的第{{love_day.DATA}}天
+    //   今天是我们相识的第{{love_day.DATA}}天
       
-      {{birthday_message.DATA}}
+    //   {{birthday_message.DATA}}
       
-      ---
+    //   ---
       
-      {{moment_copyrighting.DATA}}
+    //   {{moment_copyrighting.DATA}}
       
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
-    `
+    //   {{poetry_title.DATA}} {{poetry_content.DATA}}
+    // `
+  🗓️{{date.DATA}}
+  城市：{{city.DATA}}
+  天气☁️：{{weather.DATA}}
+  温度🌡️: {{min_temperature.DATA}}--{{max_temperature.DATA}}
+  风向：{{wind_direction.DATA}}
+  风级：{{wind_scale.DATA}}
+  温馨提示：{{notice.DATA}}
+  值得纪念：{{birthday_message.DATA}}
+  💗：{{earthy_love_words.DATA}}
+  每日一句😋
+  中文：{{note_ch.DATA}}
+  English：{{note_en.DATA}}
+  和{{poetry_author.DATA}}约个会：{{poetry_content.DATA}}
+
+    
   },
   {
     id: '0002',
